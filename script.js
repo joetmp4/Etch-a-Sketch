2a74d1d4ff6173e4202e73214332ae2394737fc2
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-22 10:33:25 -0400

    i got the hover working and the boxes (idk if i ever said i got the boxes working). overall pretty cool. all that is left is to fix the clear function, and work on the extra credit activities

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const slider = document.querySelector(".slider");
 
 const sketch = document.querySelector(".sketch");
 
+const value = document.querySelector(".value");
+
 const createGrid = () => {
     sketch.replaceChildren();
 
@@ -9,11 +11,21 @@ const createGrid = () => {
 
     for (let i = 0; i < num*num; i++){
         const cube = document.createElement("div");
-        cube.style.border = "1px solid black";
+        cube.style.border = "1px solid rgb(62, 61, 61, 0.5)";
         cube.style.width = `${100/num}%`;
         cube.style.height = `${100/num}%`;
+
+        cube.addEventListener("mouseenter", () => {
+            cube.style.backgroundColor = "black";
+        });
 
         sketch.appendChild(cube);
     }
 
+    value.textContent = `${slider.value} x ${slider.value}`;
 };
+createGrid();
+
+slider.addEventListener("input", createGrid);
+
+
