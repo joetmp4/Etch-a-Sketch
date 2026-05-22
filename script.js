281fdea932003548ddc8acc648c36f96778bd848
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-22 10:02:48 -0400

    honestly a little confused on how to create multiple boxes for the etch a sketch. ill figure it out. surely its some function method such as .map

---
+++ script.js
@@ -0,0 +1,9 @@
+const slider = document.querySelector(".slider");
+
+const sketch = document.querySelector(".sketch");
+
+const createGrid = () => {
+    const num = Number(slider.value);
+    const cube = document.createElement("div");
+    cube.style.border = "1px solid black";
+};
