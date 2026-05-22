99e9b6f43743714e9d43fa2355ba03ae8dc9d879
joet <205975513+joetmp4@users.noreply.github.com>
2026-05-22 10:09:22 -0400

    i think this is a working way to create these boxes for the etch a sketch. i have a loop to run and make all individual boxes. the boxes are sized depending on its ratio to the size of sketch

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ const slider = document.querySelector(".slider");
 const sketch = document.querySelector(".sketch");
 
 const createGrid = () => {
+    sketch.replaceChildren();
+
     const num = Number(slider.value);
-    const cube = document.createElement("div");
-    cube.style.border = "1px solid black";
+
+    for (let i = 0; i < num*num; i++){
+        const cube = document.createElement("div");
+        cube.style.border = "1px solid black";
+        cube.style.width = `${100/num}%`;
+        cube.style.height = `${100/num}%`;
+
+        sketch.appendChild(cube);
+    }
+
 };
